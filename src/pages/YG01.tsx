dce0415cf3080a8ad2014b298bbdb254a7098354
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-20 10:44:28 +0700

    feat: input stop images for YG01

--- src/pages/YG01.tsx
+++ src/pages/YG01.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import useCurrentLocation from "@/hooks/useCurrentLocation";
 import React, { useEffect, useMemo, useState } from "react";
 import dynamic from "next/dynamic";
@@ -9,17 +10,29 @@ function YG01() {
   const [centroid, setCentroid] = useState<[number, number]>([0, 0]);
   const chars: string[] = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
+  console.log("http://localhost:3000/images/" + "YG01-01.png");
+
   const markers: object[] = [];
   tour1.features.forEach((item: any, index: number) => {
     const marker = {
       position: [item.geometry.coordinates[1], item.geometry.coordinates[0]],
       iconUrl: "/" + chars[index + 1] + ".png",
       iconSize: [25, 25],
-      iconAnchor: [25, 25/4],
+      iconAnchor: [25, 25 / 4],
       popupAnchor: [12.5, 12.5],
       children: (
         <div className="font-semibold text-[16px] text-white font-poppins w-max bg-[#84899E] p-2 flex gap-[9px]">
-          <div className="w-[60px] h-[55px] bg-[#D9D9D9] rounded-[8px]"></div>
+          <div className="w-[120px] bg-[#D9D9D9] rounded-[8px] overflow-hidden">
+            <img
+              src={
+                process.env.NEXT_PUBLIC_ENV === "DEV"
+                  ? "http://localhost:3000/images/YG01-" + (item.properties.index < 10 ? "0" : "") + item.properties.index + ".png"
+                  : "https://histara-map.vercel.app/images/YG01-" + (item.properties.index < 10 ? "0" : "") + item.properties.index + ".png"
+              }
+              // src="https://drive.google.com/uc?export=view&id=1dhy2mj-fc30pPB2giZ_ztvSCINf88FuD"
+              alt={"Photo of " + item.properties.name}
+            />
+          </div>
           <p className="max-w-[120px] text-balance">{item.properties.name}</p>
           <div className="bg-white rounded-full text-black self-start size-[30px] flex justify-center items-center">
             {chars[item.properties.index]}
@@ -40,7 +53,7 @@ function YG01() {
     if (centroid[0] === 0 && centroid[1] === 0) {
       setCentroid(calculateMiddlePoint(markers));
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location, centroid]);
   const Map = useMemo(
     () =>
